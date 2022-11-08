f18611551ad10e07f4e8e6c7046c7bb77812dfe6
HalcyonAzure <2660796265@qq.com>
2022-11-08 22:28:24 +0800

    lab2 wrap & unwrap

--- libsponge/wrapping_integers.cc
+++ libsponge/wrapping_integers.cc
@@ -1,12 +1,13 @@
 #include "wrapping_integers.hh"
 
+#include "iostream"
 // Dummy implementation of a 32-bit wrapping integer
 
 // For Lab 2, please replace with a real implementation that passes the
 // automated checks run by `make check_lab2`.
 
 template <typename... Targs>
-void DUMMY_CODE(Targs &&... /* unused */) {}
+void DUMMY_CODE(Targs &&.../* unused */) {}
 
 using namespace std;
 
@@ -14,8 +15,7 @@ using namespace std;
 //! \param n The input absolute 64-bit sequence number
 //! \param isn The initial sequence number
 WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
-    DUMMY_CODE(n, isn);
-    return WrappingInt32{0};
+    return WrappingInt32(static_cast<uint32_t>(((n + isn.raw_value()) % (1ll << 32))));
 }
 
 //! Transform a WrappingInt32 into an "absolute" 64-bit sequence number (zero-indexed)
@@ -29,6 +29,20 @@ WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
 //! and the other stream runs from the remote TCPSender to the local TCPReceiver and
 //! has a different ISN.
 uint64_t unwrap(WrappingInt32 n, WrappingInt32 isn, uint64_t checkpoint) {
-    DUMMY_CODE(n, isn, checkpoint);
-    return {};
+    uint64_t value = (n.raw_value() + (1ll << 32) - isn.raw_value()) % (1ll << 32);
+    uint64_t num = checkpoint / (1ll << 32);
+
+    if (value > checkpoint) {
+        return value;
+    } else {
+        uint64_t a = value + num * (1ll << 32);
+
+        if (a > checkpoint + (1ll << 31)) {
+            return a - (1ll << 32);
+        } else if (checkpoint > a + (1ll << 31)) {
+            return a + (1ll << 32);
+        } else {
+            return a;
+        }
+    }
 }
